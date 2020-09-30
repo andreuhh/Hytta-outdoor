445efac6badba2e1af5988e428993523219601ab
Andrea Guffi <andreaguffi92@gmail.com>
2020-09-30 19:37:36 +0200

    accordion

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ burger.addEventListener('click', () => {
 })
 
 // script accordion
-var acc = document.getElementsByClassName("accordion__btn");
+/*var acc = document.getElementsByClassName("accordion__btn");
 var i;
 
 for(i = 0; i < acc.length; i++) {
@@ -21,4 +21,33 @@ for(i = 0; i < acc.length; i++) {
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     }
-}
+}*/
+
+// accordion
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+
+        const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+        if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else{
+            accordionItemBody.style.maxHeight = 0;
+        }
+    });
+});
+
+
+
+
+
+
