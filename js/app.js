e0ac8df79d8b000f1908e49270cade75d4084c60
Andrea Guffi <andreaguffi92@gmail.com>
2020-09-30 17:07:20 +0200

    aggiunto accordion

--- js/app.js
+++ js/app.js
@@ -3,3 +3,22 @@ const header = document.querySelector('.header__menu');
 burger.addEventListener('click', () => {
  header.classList.toggle('header__menu--open');
 })
+
+// script accordion
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for(i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+
+        if(panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        }
+        else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+}
